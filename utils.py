6379967567349c24c2cc71580d192f7e87503646
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-05-31 01:13:13 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def generate_deck():
+    ranks = '23456789TJQKA'
+    suits = 'cdhs'
+    return [r + s for r in ranks for s in suits]
+
+def remove_known_cards(deck, known_cards):
+    return [card for card in deck if card not in known_cards]
