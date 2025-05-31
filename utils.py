902fa393a4f5eaece1eabcfd81d98189a5e93209
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-01 01:01:09 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
+from hand_group_definitions import hand_groups
+import random
+
 def generate_deck():
-    ranks = '23456789TJQKA'
-    suits = 'cdhs'
+    """52枚のデッキを生成"""
+    ranks = "23456789TJQKA"
+    suits = "cdhs"
     return [r + s for r in ranks for s in suits]
 
 def remove_known_cards(deck, known_cards):
+    """指定されたカードをデッキから除外"""
     return [card for card in deck if card not in known_cards]
+
+def get_hand_group_dict():
+    """ハンドグループの辞書を取得"""
+    return hand_groups
